a8a43167e38ef4cbea9b3309afa291910ea4ba4f
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2025-04-25 02:41:35 +0530

    added grafana and prometheus counters for analytics

--- src/main/java/raut/shubham/analytics_consumer/model/EventData.java
+++ src/main/java/raut/shubham/analytics_consumer/model/EventData.java
@@ -3,9 +3,11 @@ package raut.shubham.analytics_consumer.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EventData {
     private String courseId;
     private String eventType;
