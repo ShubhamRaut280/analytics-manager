b23721c2b1a43b13462fac36cc6feedf3843f5f5
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2025-04-25 01:47:35 +0530

    intial

---
+++ src/test/java/raut/shubham/analytics_producer/AnalyticsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package raut.shubham.analytics_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
